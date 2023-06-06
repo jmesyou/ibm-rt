28262fa4c8e5d96043c15836ecbd7eba64b94bd1
James You <james.you@protonmail.com>
2023-06-06 23:51:51 +0000

    finalize new development environment

--- .devcontainer/scripts/fetch_jdk.sh
+++ .devcontainer/scripts/fetch_jdk.sh
@@ -8,8 +8,8 @@ cd /tmp
 JDK_VERSIONS=(8 11 17 18)
 for VERSION in ${JDK_VERSIONS[@]}; do
   FILE=jdk${VERSION}.tar.gz
-  DEST=${BOOT_JDK_ROOT}/${FILE}
-  mkdir -p {}
+  DEST=${BOOT_JDK_ROOT}/jdk${VERSION}
+  mkdir -p ${DEST}
   wget -O ${FILE} "https://api.adoptopenjdk.net/v3/binary/latest/${VERSION}/ga/linux/x64/jdk/openj9/normal/adoptopenjdk"
   tar -xzf ${FILE} --directory=${DEST} --strip-components=1
 done
